c2417ca959af0612405d8ece49878982917ac2e3
bilal <bilal0558@gmail.com>
2021-08-03 17:08:39 +0530

    Added Total Amount Field

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function computeInterest(){
-    var principleAmount = document.getElementById('principle-amount').value;
-    var interestRate = document.getElementById('interest-rate').value;
-    var years = document.getElementById('years').value;
+    var principleAmount = parseInt(document.getElementById('principle-amount').value);
+    var interestRate = parseInt(document.getElementById('interest-rate').value);
+    var years = parseInt(document.getElementById('years').value);
     
     var interest = principleAmount * (Math.pow((1 + interestRate/100),years) - 1); 
+
+    var totalAmount = principleAmount + interest;
     
     document.getElementById('interest').value = interest.toFixed(2);
+    document.getElementById('total-amount').value = totalAmount.toFixed(2);
 }
